68fb5e3356327ab4d469b81bb00a92cb1a86a061
Jose <josesantillan10@gmail.com>
2022-08-26 10:56:10 -0500

    add 3 pages and a component

---
+++ components/Navigation.js
@@ -0,0 +1,10 @@
+const Navigation = () => {
+	return(
+		<ul>
+			<li>Index</li>
+			<li>About</li>
+			<li>Services</li>
+		</ul>
+	)
+}
+export default Navigation
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Index = () =>{
+	return( <h1>Hola index</h1> )
+}
+
+export default Index
